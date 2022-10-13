758331d6cb09a21173d57319a8dc634a108d975c
Omotola Ogunsola <ogunsola40@gmail.com>
2022-10-13 23:09:36 +0100

    maxSlidingWindow example

---
+++ maxSlidingWindow.js
@@ -0,0 +1,26 @@
+// let findMaxSlidingWindow = function (nums, windowSize) {
+//   let result = [];
+//   // ................................................................
+
+//   //return expty list
+//   if (nums.length == 0) {
+//     return result;
+//   }
+
+//   //if window_size is greater than the array size,
+//   //set the window_size to nums.length()
+//   if (windowSize > nums.length) {
+//     windowSize = nums.length;
+//   }
+
+//   // ................................................................
+
+//   //Initializing doubly ended queue for storing indices using array
+//   let window = [];
+
+//   //find out max for first window
+//   for (let i = 0; i < windowSize; i++) {
+//     // For every element, remove the previous smaller elements from window
+//     while (window.length > 0 )
+//   }
+// };
