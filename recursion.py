121bd926b8cab30d2eb64db17c6f4955606808db
Omotola Ogunsola <ogunsola40@gmail.com>
2022-10-28 21:42:56 +0100

    recursion

--- recursion.py
+++ recursion.py
@@ -1,6 +1,6 @@
 def factorial(n):
     assert n>=0 and int(n) == n, 'The number must be positive integer only!'
-    if n in [0,1]:    #basecase condition
+    if n in [0,1]:    #the basecase condition
         return 1
     else:
         return n * factorial(n-1)
