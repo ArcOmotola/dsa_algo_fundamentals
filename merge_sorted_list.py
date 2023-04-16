6bc0f704b04e0a3cf0bc1c8e45ef95600bfe0753
Omotola Ogunsola <ogunsola40@gmail.com>
2023-04-16 23:54:00 +0100

    added new example

---
+++ merge_sorted_list.py
@@ -0,0 +1,12 @@
+def merge_lists(lst1, lst2):
+    # Write your code here
+    merge = lst1 + lst2
+    return merge
+
+    # merge.append(number)
+
+
+    
+
+merged_list = merge_lists([1,3,4,5], [2,6,7,8])
+print(merged_list)
