09efaa624b3710c7af8bde0733b0eb65b15627db
Omotola Ogunsola <ogunsola40@gmail.com>
2023-05-07 23:30:32 +0100

    added linear_search example

--- merge_sorted_list.py
+++ merge_sorted_list.py
@@ -1,12 +1,19 @@
-def merge_lists(lst1, lst2):
-    # Write your code here
-    merge = lst1 + lst2
-    return merge
+# def merge_lists(lst1, lst2):
+#     # Write your code here
+#     merge = lst1 + lst2
+#     merged_list = sorted(merge)
+#     return merged_list
 
-    # merge.append(number)
+#     # merge.append(number)
+
+
+#   def merge_list(lst1, lst2):
+#     index_arr1 = 0
+#     index_arr2 = 0 
+#     index_result
 
 
     
 
-merged_list = merge_lists([1,3,4,5], [2,6,7,8])
-print(merged_list)
+# merged_list = merge_lists([1,3,4,5], [2,6,7,8])
+# print(merged_list)
