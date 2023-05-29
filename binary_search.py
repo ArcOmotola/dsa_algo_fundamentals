1e3140233b870130f5f5b0166160e13e92e508bc
Omotola Ogunsola <ogunsola40@gmail.com>
2023-05-29 13:21:28 +0100

    added binary search

--- binary_search.py
+++ binary_search.py
@@ -19,3 +19,24 @@ def binary_search(list, item):
 my_list = [1, 3 , 5, 7, 9]
 
 print (binary_search(my_list, 3))
+
+
+
+
+
+
+# def binary_search(lst, item):
+#   low = 0  #lower index
+#   high = len(lst) - 1 #upper index
+
+#   while low <= high :  #as long as there is still a search space
+#     mid = low + high // 2 #index of mid value
+
+#     if lst[mid] == item:
+#       return mid
+#     if lst[mid] > item:
+#       high = mid - 1 
+#     else:
+#       low = mid + 1
+#   return None
+
