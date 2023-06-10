be6f31ac093d7b725cdc5ee23ac3c36608ce4afc
Omotola Ogunsola <ogunsola40@gmail.com>
2023-06-10 23:17:05 +0100

    added bubble sort example

--- test.py
+++ test.py
@@ -1,22 +1,10 @@
-def find_smallest_index(arr):
-  lowest = arr[0]
-  lowest_index = 0
-  
-  for i in range(1, len(arr)):
-    if arr[i] < lowest:
-      lowest = arr[i]
-      lowest_index = i
-  return lowest_index
-
-
-def selectionSort(arr):
-  sorted = []
-  while len(arr) > 0:
-    lowest = find_smallest_index(arr)
-    sorted.append(arr.pop(lowest))
-  return sorted
+def bubble_sort(arr):
 
 
 
 
-print (selectionSort([5, 20, 3, 1, 2, 10, 10, 4]))
+
+
+
+
+# print (quicksort([10, 5, 2, 3, 10, 7, 3, 4]))
