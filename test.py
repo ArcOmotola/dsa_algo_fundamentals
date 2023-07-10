946b49e96147361eabdcf567eba22931c7ff3f3e
ArcOmotola <ogunsola40@gmail.com>
2023-07-10 23:32:04 +0100

    added subset example

--- test.py
+++ test.py
@@ -1,10 +1,45 @@
-# Given an array of integers, find the longest increasing subsequence in the array. 
-# A subsequence is a sequence of elements in the array that are in order, but not necessarily contiguous.
-
-
-# Given an integer array nums, return the length of the longest strictly increasing 
-# subsequence
-
-def lengthOfLIS(nums):
+# def subsets(nums):
+#     result = []
     
+#     def backtrack(start, current_subset):
+#         result.append(current_subset[:])
+        
+#         for i in range(start, len(nums)):
+#             current_subset.append(nums[i])
+#             backtrack(i + 1, current_subset)
+#             current_subset.pop()
+    
+#     backtrack(0, [])
+#     return result
 
+
+# nums = [1, 2, 3]
+# result = subsets(nums)
+# print(result)
+
+
+
+def subsets(nums):
+    result = []
+    
+    print("result>>", result)
+    
+    def backtrack(start, current_subset):
+        print("params>>", start, current_subset)
+        result.append(current_subset[:])
+        # print("result_after>>", result)
+        
+        for i in range(start, len(nums)):
+            current_subset.append(nums[i])
+            backtrack(i + 1, current_subset)
+            # print("before pop>>", current_subset)
+            current_subset.pop()
+            # print("after pop>>", current_subset)
+    
+    backtrack(0, [])
+    return result
+
+
+nums = [1, 2, 3]
+result = subsets(nums)
+print(result)
