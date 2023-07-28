76ba5469e0ed86e7ac16ddb7b899b86d2b51cb18
ArcOmotola <ogunsola40@gmail.com>
2023-07-28 07:01:57 +0100

    added binary search recursive approach

--- binary_search.py
+++ binary_search.py
@@ -40,3 +40,21 @@ print (binary_search(my_list, 5))
 #       low = mid + 1
 #   return None
 
+
+
+
+
+
+# RECURSIVE APPROACH
+
+def binary_search_recursive(data, target, low, high):
+	if low > high:
+		return False
+	else:
+		mid = (low + high) // 2
+		if target == data[mid]:
+			return True
+		elif target < data[mid]:
+			return binary_search_recursive(data, target, low, mid-1)
+		else:
+			return binary_search_recursive(data, target, mid+1, high)
