0bbb05d97a034f517c7bbc2f8ea2aecd32f41b40
ArcOmotola <ogunsola40@gmail.com>
2023-08-23 23:19:34 +0100

    added binary search recursive approach

--- test.py
+++ test.py
@@ -1,111 +1,148 @@
 
-    
+# #brute force solution
 
-# # def two_sum(nums, target):
-# #   """
-# #   Finds two numbers in the array that add up to the target number.
+# def findMaxConsecutiveOnes(nums):
+#   count = 0
+#   max_count = 0
 
-# #   Args:
-# #     nums: The array of numbers.
-# #     target: The target number.
-
-# #   Returns:
-# #     A list of two numbers in the array that add up to the target number.
-# #   """
-# #   hash_table = {}
-# #   for i in range(len(nums)):
-# #     compliment = target - nums[i]
-# #     if compliment in hash_table:
-# #       return [i, hash_table[compliment]]
-# #     else:
-# #       hash_table[nums[i]] = i
-
-# #   return None
-
-
-
-
-
-
-# # print(two_sum([3,2,4,5,7], 9))
-
-
-
-
-# # def two_sum(nums, target):
-    
-# #     # This line of code creates a hash table to store the numbers in the array.
-# #     # The hash table is a data structure that allows us to quickly look up whether a given number is in the array.    
-# #     hash_table = {}
-
-# #     for i in range(len(nums)):     # This line of code iterates through the array once.
-# #         compliment = target - nums[i]     # This line of code calculates the compliment of the target number.
-# #         if compliment in hash_table:
-# #             return [i, hash_table[compliment]]
-# #         else:
-# #             hash_table[nums[i]] = i
-
-# #     return None
-
-
-
-
-# def two_sum(nums, target):
-#     hash_table = {}
-
-#     for i in range(len(nums)):
-#       compliment = target - nums[i]
-
-#       if compliment in hash_table and hash_table[compliment] != i:
-#         return [i, hash_table[compliment]]
+#   for i in range(len(nums)):
+#       if nums[i] == 1:
+#           count += 1
 #       else:
-#         hash_table[nums[i]] = i
-    
-#     return None
-
-
-
-
-
-
-
-def findMaxConsecutiveOnes(nums):
-  count = 0
-  curr_max = 0
-  # current_count = 0
-  # count = {}
-  for i in range(len(nums)):
-      if nums[i] == 1:
-          count += 1
-      # if nums[i] == 0:
-      else:
-          max_count = 
-          old_max = count
-          count[i] = 0
-
-          if nums[i+1] != 1 and i < len(nums) - 1:
-              curr_max = count
+#           max_count = max(max_count, count)
+#           count = 0
+#   max_count = max(max_count, count)
+#   return max_count
 
   
-  #         if nums[i+1] != 1:
-  #             current_max = count
-  # return max(count, current_max)
+
+# def max_consecutive_ones(nums):
+#     max_count = 0
+#     count = 0
+#     for num in nums:
+#         if num == 1:
+#             count += 1
+#             max_count = max(max_count, count)
+#         else:
+#             count = 0
+#     return max_count
 
 
 
-# print(findMaxConsecutiveOnes([1,1,0,1,1,1]))
 
-def max_consecutive_ones(nums):
-    max_count = 0
-    count = 0
-    for i in range(len(nums)):
-        if nums[i] == 1:
-            count += 1
+# print(max_consecutive_ones([1,1,0,1,1,1]))
+
+
+
+
+
+# #sliding window solution
+
+# def max_consecutive_ones(nums):
+#     max_count = 0
+#     window_start = 0
+#     for window_end in range(len(nums)):
+#         if nums[window_end] == 0:
+#             window_start = window_end + 1
+#         max_count = max(max_count, window_end - window_start + 1)
+#     return max_count
+
+
+
+
+# #counting groups
+
+# def max_consecutive_ones(nums):
+#     max_count = 0
+#     count = 0
+#     for num in nums:
+#         if num == 1:
+#             count += 1
+#         else:
+#             max_count = max(max_count, count)
+#             count = 0
+#     max_count = max(max_count, count)
+#     return max_count
+
+
+
+# #using str.split('0')
+
+# def max_consecutive_ones(nums):
+#     bin_str = ''.join(map(str, nums))
+#     ones_groups = bin_str.split('0')
+#     max_count = max(map(len, ones_groups))
+#     return max_count
+
+
+
+# #using bit manipulation
+
+# def max_consecutive_ones(nums):
+#     max_count = 0
+#     current_count = 0
+#     for num in nums:
+#         if num == 1:
+#             current_count += 1
+#             max_count = max(max_count, current_count)
+#         else:
+#             current_count = 0
+#     return max_count
+
+
+
+
+
+    
+# def duplicate_zeros(arr):
+#         zeroes = arr.count(0)
+#         n = len(arr)
+#         for i in range(n-1, -1, -1):
+#             if i + zeroes < n:
+#                 arr[i + zeroes] = arr[i]
+#             if arr[i] == 0: 
+#                 zeroes -= 1
+#                 if i + zeroes < n:
+#                     arr[i + zeroes] = 0
+
+
+# print(duplicate_zeros([1,0,2,3,0,4,5,0]))
+
+
+def duplicate_zeros(arr):
+        zeroes = arr.count(0)
+        n = len(arr)
+        for i in range(n-1, -1, -1):
+            if i + zeroes < n:
+                arr[i + zeroes] = arr[i]
+            if arr[i] == 0: 
+                zeroes -= 1
+                if i + zeroes < n:
+                    arr[i + zeroes] = 0
+
+
+def duplicate_zeros(arr):
+    n = len(arr)
+    result = []
+    
+    for i in range(n):
+        if arr[i] == 0:
+            result.append(0)
+            result.append(0)
         else:
-            max_count = max(max_count, count)
-            count = 0
-    max_count = max(max_count, count)
-    return max_count
+            result.append(arr[i])
+            
+
+    for i in range(n):
+        arr[i] = result[i]
+
+array = [1, 0, 2, 3, 0, 4, 5, 0]
+
+duplicate_zeros(array)
+
+print(array)
 
 
-print(max_consecutive_ones([1,1,0,1,1,1]))
+
+
+print("length>>>", len([1,2,3,0,0,0]))
