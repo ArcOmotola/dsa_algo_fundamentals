a6078ff48a9f3b38220a05c877f1000999ab98d4
ArcOmotola <ogunsola40@gmail.com>
2023-08-26 23:52:46 +0100

    added example

--- test.py
+++ test.py
@@ -121,28 +121,67 @@ def duplicate_zeros(arr):
                     arr[i + zeroes] = 0
 
 
-def duplicate_zeros(arr):
-    n = len(arr)
-    result = []
+# def duplicate_zeros(arr):
+#     n = len(arr)
+#     result = []
     
-    for i in range(n):
-        if arr[i] == 0:
-            result.append(0)
-            result.append(0)
-        else:
-            result.append(arr[i])
+#     for i in range(n):
+#         if arr[i] == 0:
+#             result.append(0)
+#             result.append(0) 
+#         else:
+#             result.append(arr[i])
             
 
-    for i in range(n):
-        arr[i] = result[i]
+#     for i in range(n):
+#         arr[i] = result[i]
 
-array = [1, 0, 2, 3, 0, 4, 5, 0]
+# array = [1, 0, 2, 3, 0, 4, 5, 0]
 
-duplicate_zeros(array)
+# duplicate_zeros(array)
+
+# print(array)
+
+
+
+
+# print("length>>>", len([1,2,3,0,0,0]))
+
+
+
+
+nums1 = [0, 3, 5, 1, 8, 7]
+nums2 = [9, 5, 2, 4]
+
+result = nums1 + nums2
+
+result = sorted(result)
+
+print(result)
+
+
+def merge(nums1, m, nums2, n):
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+      
+        result = nums1 + nums2
+        result = sorted(result)
+        
+        for i in range(len(m)):
+            if result[i] > 0:
+                nums1[i] = result[i]
+
+
+
+def modify_array(array):
+    pointer = array
+    while pointer:
+        pointer[0] = 0
+        pointer = pointer[1:]
+
+array = [1, 2, 3, 4, 5]
+
+modify_array(array)
 
 print(array)
-
-
-
-
-print("length>>>", len([1,2,3,0,0,0]))
