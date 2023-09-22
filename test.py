aab367c8563db75105fc8e313b026e41d14b6574
ArcOmotola <ogunsola40@gmail.com>
2023-09-22 23:45:57 +0100

    edited test.py

--- test.py
+++ test.py
@@ -1,103 +1,81 @@
-def longest_common_prefix(strs):
-    if not strs:
-        return ""
+# def longestCommonPrefix(strs):
+#     # Check for an empty input list
+#     if not strs:
+#         return ""
 
-    # Sort the array to ensure the shortest string comes first.
-    strs.sort()
-
-    # Consider the first and last strings (since they are sorted).
-    first_str = strs[0]
-    last_str = strs[-1]
-
-    # Find the common prefix between the first and last strings.
-    common_prefix = []
-    for i in range(len(first_str)):
-        if i < len(last_str) and first_str[i] == last_str[i]:
-            common_prefix.append(first_str[i])
-        else:
-            break
-
-    return "".join(common_prefix)
-
-# Example usage:
-strings = ["flower", "flour", "flourish"]
-# result = longest_common_prefix(strings)
-# print(result)  # Output will be "flo"
-# print(min(strings, key=len))
-
-
-
-
-def longestCommonPrefix(strs):
-    # Check for an empty input list
-    if not strs:
-        return ""
-
-    # Find the shortest string in the list using the min function
-    shortest = min(strs, key=len)
+#     # Find the shortest string in the list using the min function
+#     shortest = min(strs, key=len)
     
-    # Initialize an empty prefix string
-    prefix = ""
+#     # Initialize an empty prefix string
+#     prefix = ""
 
-    # Iterate through the characters of the shortest string
-    for i in range(len(shortest)):
-        char = shortest[i]
+#     # Iterate through the characters of the shortest string
+#     for i in range(len(shortest)):
         
-        # Check if the character exists in the same position of all other strings
-        for s in strs:
-            if s[i] != char:
-                return prefix  # If not, return the current prefix
-        prefix += char  # If it exists in all strings, add it to the prefix
+#         # Check if the character exists in the same position of all other strings
+#         for s in strs:
+#             if s[i] != shortest[i]:
+#                 return prefix  # If not, return the current prefix
+#         prefix += shortest[i]  # If it exists in all strings, add it to the prefix
 
-    return prefix
-
-
-
-strings = ["flower", "flour", "flourish"]
-result = longestCommonPrefix(strings)
-print(result)  # Output will be "flo"
+#     return prefix
 
 
+
+# strings = ["flower", "flour", "flourish"]
+# result = longestCommonPrefix(strings)
+# print(result)  # Output will be "flo"
 
 
 
 
 
-def longest_common_prefix(strings):
-  """
-  Finds the longest common prefix string amongst an array of strings.
-
-  Args:
-    strings: An array of strings.
-
-  Returns:
-    The longest common prefix string, or an empty string "" if there is no common prefix.
-  """
-
-  if not strings:
-    return ""
-
-  shortest_string = min(strings, key=len)
-  prefix = ""
-  for i in range(len(shortest_string)):
-    matched = True
-    for string in strings:
-      if string[i] != shortest_string[i]:
-        matched = False
-        break
-
-    if not matched:
-      break
-
-    prefix += shortest_string[i]
-
-  return prefix
 
 
-def main():
-  strings = ["doser", "door", "done"]
-  print(longest_common_prefix(strings))
+my_list = ["geye", "hdjdhdh", "jjsgshsh", "bshsgs", "bdjdjd"]
+
+# Find the index of an item in the list
+index = my_list.index("jjsgshsh")
+
+print("Index of 30:", index)
 
 
-if __name__ == "__main__":
-  main()
+
+
+
+
+
+
+# list1_set = set(List1)
+#         List2_set = set(List2)
+        
+#         common_set = List1_set.intersection(List2_set)
+        
+#         common_items = List(common_set)
+        
+#         least_sum = 5000
+        
+#         for i in range(len(common_items)):
+#             index_sum = List1.index(common_items[i])  + List2.index(common_items[i])
+#             if index_sum < least_sum:
+#                 least_sum = index_sum
+                
+#         return least_sum
+
+
+master = ["Burger King","KFC"]
+arr1 = ["Shogun","Tapioca Express","Burger King","KFC"]
+arr2 = ["KFC","Shogun", "Burger King", "Tapioca Express"]
+
+hash = { i :  len(i) for i  in arr2 if i in master }
+arr = [i for i in arr2 if i in master]
+print("hashmap>>", hash)
+print("array>>>", arr)
+
+# print(hash.items())
+
+def iterate(hash):
+    for key, value in hash.items():
+        print("iterate>>", value)
+
+iterate(hash)
