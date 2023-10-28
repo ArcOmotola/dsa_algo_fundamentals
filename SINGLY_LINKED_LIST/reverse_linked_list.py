d8f631544df6e17b23b74d46cc3521e5ae4ae83c
ArcOmotola <ogunsola40@gmail.com>
2023-10-28 14:13:38 +0100

    added reverse linked list

--- SINGLY_LINKED_LIST/reverse_linked_list.py
+++ SINGLY_LINKED_LIST/reverse_linked_list.py
@@ -1,29 +1,11 @@
-def traverse_circular_linked_list(head):
-  """Traverses a circular linked list.
+def reverse_linked_list(head):
+  previous = None
+  current = head   
 
-  Args:
-    head: The head of the circular linked list.
-  """
+  while current is not None:
+    next = current.next
+    current.next = previous
+    previous = current
+    current = next
 
-  current_node = head
-
-  while current_node != head:
-    print(current_node.val)
-    current_node = current_node.next
-
-  print("222>>",current_node.val)
-
-
-
-class Node:
-  def __init__(self, val = 0):
-    self.val = val
-    self.next = None
-
-head = Node(1)
-head.next = Node(2)
-head.next.next = Node(3)
-head.next.next.next = head
-
-# The circular linked list is traversed and the data of each node is printed.
-traverse_circular_linked_list(head)
+  return previous
--- test.py
+++ test.py
@@ -1,35 +1,11 @@
-def modify_linked_list(head, modify_function):
-  # Traverse the linked list and modify the data of each node as needed.
-  current = head
+def reverse_linked_list(head):
+  previous = None
+  current = head   
+
   while current is not None:
-    current = modify_function(current)
-    current = current.next
-  
-  # Return the head of the modified linked list.
-  return head
+    next = current.next
+    current.next = previous
+    previous = current
+    current = next
 
-
-
-"""The modify_function can be any function that takes a node as 
-input and returns the modified node. For example, we could use the 
-following modify_function to double the value of each node in the linked list:"""
-
-def double_value(node):
-  node.data *= 2
-  return node
-
-
-
-# ///////////// 
-
-def insert_new_node(node):
-  new_node = Node(node.data + 1)
-  new_node.next = node.next
-  node.next = new_node
-  return node
-
-
-class Node:
-  def __init__(self, data):
-    self.data = data
-    self.next = None
+  return previous
